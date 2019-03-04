f969858a7f5f6548907c78b8a63ea77cc4d62f24
qilianshan <qilianshan@xiaomi.com>
2019-03-04 14:30:19 +0800

    add file
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/ppp/LinkedListComponents817.java
+++ src/main/java/ppp/LinkedListComponents817.java
@@ -1,7 +1,6 @@
 package ppp;
 
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Set;
 
 public class LinkedListComponents817 {
@@ -11,26 +10,24 @@ public class LinkedListComponents817 {
 	 * ListNode(int x) { val = x; } }
 	 */
 	public static void main(String[] args) {
-		LinkedListComponents817 linkedListComponents817=new LinkedListComponents817();
-
+		LinkedListComponents817 linkedListComponents817 = new LinkedListComponents817();
+		ListNode node = ListNode.CreateListByArr(new int[]{0, 1, 2, 3});
+		int i = linkedListComponents817.numComponents(node, new int[]{0, 1, 3});
 	}
-	class Solution {
 
-		public int numComponents(ListNode head, int[] G) {
-			Set<Integer> curSet = new HashSet<>();
-			for (int i = 0; i < G.length; i++) {
-				curSet.add(G[i]);
-			}
-			int count = 0;
-			ListNode pre = null;
-			while (head.next != null) {
-				if (!curSet.contains(pre) && curSet.contains(head)) {
-					count++;
-				}
-				pre = head;
-				head = head.next;
-			}
-			return count;
+	public int numComponents(ListNode head, int[] G) {
+		Set<Integer> curSet = new HashSet<>();
+		for (int i = 0; i < G.length; i++) {
+			curSet.add(G[i]);
 		}
+		int count = 0;
+		while (head != null) {
+			if (curSet.contains(head.val) && (head.next == null || !curSet
+				.contains(head.next.val))) {
+				count++;
+			}
+			head = head.next;
+		}
+		return count;
 	}
 }
--- src/main/java/ppp/PalindromicSubstrings.java
+++ src/main/java/ppp/PalindromicSubstrings.java
@@ -7,38 +7,52 @@ public class PalindromicSubstrings {
 
 	public static void main(String[] args) {
 		PalindromicSubstrings palindromicSubstrings = new PalindromicSubstrings();
-		int f = palindromicSubstrings.countSubstrings("aaa");
-		int s = palindromicSubstrings.countSubstrings("abc");
+		System.out.println(palindromicSubstrings.countSubstrings("abc"));
 	}
 
 	public int countSubstrings(String s) {
-		int res = 0;
+		//预处理，处理后；原文回文长度为半径减1
+		//@为了处理边界情况
+		Character[] characters = new Character[s.length() * 2 + 2];
+		characters[0] = '@';
 		for (int i = 0; i < s.length(); i++) {
-			res += judge(i, s);
+			characters[(i + 1) * 2 - 1] = '#';
+			characters[(i + 1) * 2] = s.charAt(i);
 		}
-		return res;
-	}
-
-	public int judge(int i, String s) {
-		int count = 1;
-		//以自己为中心
-		int offset = 1;
-		while (i - offset >= 0 && i + offset < s.length() && s.charAt(i - offset) == s
-			.charAt(i + offset)) {
-			count++;
-			offset++;
-		}
-		//以自己和下一个为中心
-		if (i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1)) {
-			count++;
-			offset = 1;
-			while (i - offset >= 0 && i + 1 + offset < s.length() && s.charAt(i - offset) == s
-				.charAt(i + 1 + offset)) {
-				count++;
-				offset++;
+		characters[characters.length - 1] = '#';
+		int right = 1;
+		int center = 1;
+		int[] lenRecord = new int[characters.length];
+		for (int i = 1; i < characters.length; i++) {
+			int curLen = 0;
+			//两种情况，在目前的搜索过的最大范围之内
+			if (i >= right) {
+				//以目前位置距离right的大小和镜像位置的记录的较小的值为起始记录
+				lenRecord[i] = Math.min(lenRecord[center * 2 - i], right - i + 1);
+				//不在目前的范围内
+			} else {
+				lenRecord[i] = 1;
+			}
+			//计算目前位置的长度
+			while (i - lenRecord[i] >= 0 && i + lenRecord[i] < characters.length &&
+				characters[i - lenRecord[i]] == characters[i + lenRecord[i]]) {
+				lenRecord[i]++;
+			}
+			//更新坐标轴
+			if (i + lenRecord[i] > right) {
+				right = i + lenRecord[i] - 1;
+				center = i;
 			}
 		}
-		return count;
+		int res = 0;
+		for (int i = 0; i < characters.length; ++i) {
+			if (characters[i] == '#') {
+				res += (lenRecord[i] - 1) / 2;
+			} else {
+				res += (lenRecord[i]) / 2;
+			}
+		}
+		return res;
 	}
 
 }
