b42e80af41c2e2d67d095ac23b98444b3032c7a5
qilianshan <qilianshan@xiaomi.com>
2019-03-26 11:35:08 +0800

    bfs
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/ppp/WordLoadder127.java
+++ src/main/java/ppp/WordLoadder127.java
@@ -9,23 +9,64 @@ import java.util.Map;
  * Created by 祁连山 on 2019/3/16.
  */
 public class WordLoadder127 {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        boolean[] used=new boolean[]{};
 
-        return 0;
-    }
+	/**
+	 * beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+	 */
+	public static void main(String[] args) {
+		List<String> wordList = new ArrayList<>();
+		wordList.add("hot");
+		wordList.add("dog");
+		wordList.add("dot");
+		wordList.add("lot");
+		wordList.add("log");
+		wordList.add("cog");
+		WordLoadder127 wordLoadder127 = new WordLoadder127();
+		wordLoadder127.ladderLength("hit", "cog", wordList);
+	}
 
-    public boolean find(String a,String b){
-        return true;
-    }
+	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+		if (!wordList.contains(endWord)) {
+			return 0;
+		}
+		wordList.remove(endWord);
+		boolean[] used = new boolean[wordList.size()];
+		int[] distanceStart = new int[wordList.size()];
+		int[] distanceEnd = new int[wordList.size()];
+		getDistance(wordList, used, distanceEnd, endWord, 1);
+		getDistance(wordList, used, distanceStart, beginWord, 1);
+		int res = 0;
+		for (int i = 0; i < distanceEnd.length; i++) {
+			if (distanceStart[i] != 0 && distanceEnd[i] != 0) {
+				res += distanceEnd[i] + distanceStart[i];
+			}
+		}
+		return res;
+	}
 
-    public boolean ifOneLetterDif(String a, String b) {
-        int count = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) == b.charAt(i)) {
-                count++;
-            }
-        }
-        return count == a.length() - 1;
-    }
+	private int size = Integer.MAX_VALUE;
+
+	public void getDistance(List<String> wordList, boolean[] used, int[] distance,
+		String currentWord, int depth) {
+		if (depth > wordList.size() / 2) {
+			return;
+		}
+		for (int i = 0; i < wordList.size(); i++) {
+			if (ifOneLetterDif(currentWord, wordList.get(i)) && !used[i]) {
+				used[i] = true;
+				distance[i] = Math.min(depth, distance[i]);
+				used[i] = false;
+			}
+		}
+	}
+
+	public boolean ifOneLetterDif(String a, String b) {
+		int count = 0;
+		for (int i = 0; i < a.length(); i++) {
+			if (a.charAt(i) == b.charAt(i)) {
+				count++;
+			}
+		}
+		return count == a.length() - 1;
+	}
 }
