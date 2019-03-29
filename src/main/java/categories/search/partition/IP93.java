cae216a5ffff4f85521cc7edb52f8223c3d9e1ff
qilianshan <qilianshan@xiaomi.com>
2019-03-29 14:37:55 +0800

    IP
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/search/partition/IP93.java
+++ src/main/java/categories/search/partition/IP93.java
@@ -1,7 +1,10 @@
 package categories.search.partition;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public class IP93 {
 
@@ -10,37 +13,71 @@ public class IP93 {
 
 	public static void main(String[] args) {
 		IP93 ip93 = new IP93();
+		ip93.restoreIpAddresses("1111");
 		ip93.restoreIpAddresses("25525511135");
+		ip93.restoreIpAddresses("0000");
+		ip93.restoreIpAddresses("010010");
 	}
 
 	public List<String> restoreIpAddresses(String s) {
-		int[] groups = new int[]{1, 2, 3};
-		List<String> res = new ArrayList<>();
-		search(res, new ArrayList<>(), groups, s, 0);
+		res.clear();
+		if (s.length() < 4) {
+			return res;
+		}
+		int[] groups = new int[]{1, 1, 1};
+		search(new ArrayList<>(), groups, s, 0);
 		return res;
 	}
 
-	public void search(List<String> res, List<Integer> pivot, int[] groups, String s,
+	List<String> res = new ArrayList<>();
+
+	public void search(List<String> record, int[] groups, String s,
 		int currentSeperator) {
 		if (currentSeperator == 3) {
-			int currentInt = Integer.parseInt(s.substring(groups[2], s.length()));
-			if (currentInt > 0 && currentInt < 256) {
-				for (int i = 0; i < pivot.size(); i++) {
-
-				}
+			if (groups[2] >= s.length()) {
+				return;
 			}
+			String subStr = s.substring(groups[2], s.length());
+			record.add(subStr);
+			if (legal(subStr)) {
+				res.add(String.join(".", record));
+			}
+			record.remove(record.size() - 1);
+			return;
 		}
 		for (int i = 0; i < 3; i++) {
 			groups[currentSeperator] += i;
-			pivot.add(groups[currentSeperator]);
-			int lastSeperator = currentSeperator == 0 ? 0 : currentSeperator - 1;
-			int currentValue = Integer
-				.parseInt(s.substring(groups[lastSeperator], groups[currentSeperator]));
-			if (currentValue > 0 && currentValue < 256) {
-				search(res, pivot, groups, s, currentSeperator + 1);
+			if (currentSeperator + 1 < 3) {
+				groups[currentSeperator + 1] = groups[currentSeperator] + 1;
 			}
+			int lastSperatorIndex = currentSeperator == 0 ? 0 : groups[currentSeperator - 1];
+			if (groups[currentSeperator] == s.length()) {
+				break;
+			}
+			String subStr = s.substring(lastSperatorIndex, groups[currentSeperator]);
+			record.add(subStr);
+			//在目前这个值付合规则
+			if (legal(subStr) &&
+				//余下的位数不超过最大长度
+				((s.length() - groups[currentSeperator]) <= (4 - (currentSeperator + 1)) * 3)) {
+				search(record, groups, s, currentSeperator + 1);
+			}
+			record.remove(record.size() - 1);
 			groups[currentSeperator] -= i;
-			pivot.remove(pivot.size() - 1);
 		}
+	}
+
+	public boolean legal(String s) {
+		if (s.length() == 0) {
+			return false;
+		}
+		if (s.charAt(0) == '0' && s.length() > 1) {
+			return false;
+		}
+		int currentInt = Integer.parseInt(s);
+		if (currentInt < 0 || currentInt > 255) {
+			return false;
+		}
+		return true;
 	}
 }
