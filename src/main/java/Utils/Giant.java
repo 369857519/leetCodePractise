c02a75f291e916f41eac984828eac1058dd1b060
qilianshan <qilianshan@xiaomi.com>
2019-05-16 15:56:18 +0800

    add file
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/Utils/Giant.java
+++ src/main/java/Utils/Giant.java
@@ -1,5 +1,7 @@
 package Utils;
 
 public class Giant {
+	//不会做的
 	//162
+	//378
 }
---
+++ src/main/java/categories/BinarySearch/Sqrt69.java
@@ -0,0 +1,35 @@
+package categories.BinarySearch;
+
+public class Sqrt69 {
+
+	public static void main(String[] args) {
+		Sqrt69 sqrt69=new Sqrt69();
+		int res=0;
+			res=sqrt69.mySqrt(2147395599);
+	}
+	public int mySqrt(int x) {
+		int l=0;int r=x;
+		while(l<r){
+			long mid=l+r>>>1;
+			int intMid=(int)mid;
+			long c=mid*mid;
+			long n=(mid+1)*(mid+1);
+			//小于0超过了int最大值，肯定比x大
+			if(c>x){
+				r=intMid;
+			}else if (c==x){
+				return intMid;
+			}else if (c<x){
+				//小于0超过了int最大值，肯定比x大
+				if(n>x){
+					return intMid;
+				}
+				if(n==x){
+					return intMid+1;
+				}
+				l=intMid+1;
+			}
+		}
+		return l;
+	}
+}
