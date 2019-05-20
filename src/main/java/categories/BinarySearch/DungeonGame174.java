48b813380caff10f49884206734976b0d11ff9e3
qilianshan <qilianshan@xiaomi.com>
2019-05-20 11:37:02 +0800

    dp
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/BinarySearch/DungeonGame174.java
+++ src/main/java/categories/BinarySearch/DungeonGame174.java
@@ -1,69 +1,93 @@
 package categories.BinarySearch;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
 /**
  * Created by 祁连山 on 2019/5/19.
  */
 public class DungeonGame174 {
-    public static void main(String[] args) {
-        DungeonGame174 dungeonGame174 = new DungeonGame174();
-        int res = dungeonGame174.calculateMinimumHP(new int[][]{
-                {3, 0, -3},
-                {-3, -2, -2},
-                {3, 1, -5}
-        });
-    }
 
-    private int m = 0;
-    private int n = 0;
+	public static void main(String[] args) {
+		DungeonGame174 dungeonGame174 = new DungeonGame174();
+		int res = dungeonGame174.calculateMinimumHP(new int[][]{
+			{3, 0, -3},
+			{-3, -2, -2},
+			{3, 1, -5}
+		});
 
-    public int calculateMinimumHP(int[][] dungeon) {
-        m = dungeon.length;
-        if (m == 0) {
-            return 1;
-        }
-        n = dungeon[0].length;
-        if (n == 0) {
-            return 1;
-        }
-        int min = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int k = dungeon[i][j];
-                if (k < 0) min += k;
-            }
-        }
-        int l = 1, h = Math.abs(min) + 1;
-        while (l < h) {
-            int mid = l + h >>> 1;
-            if (traverse(mid, dungeon, new int[m][n], 0, 0)) {
-                h = mid;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return l;
-    }
+		res = dungeonGame174.calculateMinimumHP(new int[][]{
+			{-2, -3, 3},
+			{-5, -10, 1},
+			{10, 30, -5}
+		});
 
-    private int[][] dir = new int[][]{{0, 1}, {1, 0}};
+		res = dungeonGame174.calculateMinimumHP(new int[][]{
+			{-3,5}
+		});
+	}
 
-    public boolean traverse(int currentVal, int[][] grid, int[][] record, int x, int y) {
-        currentVal += grid[x][y];
-        if (currentVal > 0 && currentVal > record[x][y]) {
-            record[x][y] = currentVal;
-            if (x == m - 1 && y == n - 1 && currentVal > 0) {
-                return true;
-            }
-            boolean res = false;
-            for (int i = 0; i < dir.length; i++) {
-                int nx = x + dir[i][0];
-                int ny = y + dir[i][1];
-                if ((nx < m && ny < n) &&
-                        traverse(currentVal, grid, record, nx, ny)) {
-                    res = true;
-                }
-            }
-            return res;
-        }
-        return false;
-    }
+
+	public int calculateMinimumHP(int[][] dungeon) {
+		int m = dungeon.length;
+		if (m == 0) {
+			return 1;
+		}
+		int n = dungeon[0].length;
+		if (n == 0) {
+			return 1;
+		}
+		int min = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				int k = dungeon[i][j];
+				if (k < 0) {
+					min += k;
+				}
+			}
+		}
+		int l = 1, h = Math.abs(min) + 1;
+		while (l < h) {
+			int mid = l + h >>> 1;
+			if (traverse(mid, dungeon, new int[m][n], m, n)) {
+				h = mid;
+			} else {
+				l = mid + 1;
+			}
+		}
+		return l;
+	}
+
+	public boolean traverse(int initial, int[][] grid, int[][] record, int m, int n) {
+		//初始化
+		int[][] dir = new int[][]{{0, 1}, {1, 0}};
+		Queue<Integer[]> queue = new LinkedList<>();
+		queue.offer(new Integer[]{0, 0});
+		record[0][0] = grid[0][0] + initial;
+		if(record[0][0]<=0)return false;
+		//开始遍历
+		while (!queue.isEmpty()) {
+			Integer[] currentPosition = queue.poll();
+			for (int i = 0; i < dir.length; i++) {
+				int nx = dir[i][0] + currentPosition[0];
+				int ny = dir[i][1] + currentPosition[1];
+				if (nx < m && ny < n) {
+					int nextRecord = record[nx][ny];
+					int newNexRecord =
+						record[currentPosition[0]][currentPosition[1]] + grid[nx][ny];
+					if (newNexRecord > nextRecord) {
+						record[nx][ny] = newNexRecord;
+						queue.offer(new Integer[]{nx, ny});
+						if (nx == m - 1 && ny == n - 1 && newNexRecord > 0) {
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
+	}
 }
---
+++ src/main/java/categories/dp/DungeonGame174DP.java
@@ -0,0 +1,5 @@
+package categories.dp;
+
+public class DungeonGame174DP {
+
+}
