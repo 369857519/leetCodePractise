3bf498c2b6dfd1aa41e6c6965601e50677582107
qilianshan <qilianshan@xiaomi.com>
2019-05-21 17:40:42 +0800

    src
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

---
+++ src/main/java/categories/BST/MinAbsoluteDistance530.java
@@ -0,0 +1,29 @@
+package categories.BST;
+
+import Utils.TreeNode;
+import java.util.Stack;
+
+public class MinAbsoluteDistance530 {
+
+	public static void main(String[] args) {
+		TreeNode treeNode=TreeNode.createTreeByArr(new int[]{7,3,8});
+		MinAbsoluteDistance530 minAbsoluteDistance530=new MinAbsoluteDistance530();
+		minAbsoluteDistance530.getMinimumDifference(treeNode);
+	}
+
+	Stack<Integer> stack = new Stack<>();
+
+	public int getMinimumDifference(TreeNode root) {
+		if (root == null) {
+			return Integer.MAX_VALUE;
+		}
+		int leftMin = getMinimumDifference(root.left);
+		int current = Integer.MAX_VALUE;
+		if (!stack.empty()) {
+			current = root.val - stack.peek();
+		}
+		stack.push(root.val);
+		int rightMin = getMinimumDifference(root.right);
+		return Math.min(Math.min(leftMin, current), rightMin);
+	}
+}
---
+++ src/main/java/categories/BST/SearchNode700.java
@@ -0,0 +1,20 @@
+package categories.BST;
+
+import Utils.TreeNode;
+
+public class SearchNode700 {
+
+	public TreeNode searchBST(TreeNode root, int val) {
+		if (root == null) {
+			return null;
+		}
+		if (root.val == val) {
+			return root;
+		} else if (val > root.val) {
+			return searchBST(root.right, val);
+		} else {
+			//左子树
+			return searchBST(root.left, val);
+		}
+	}
+}
