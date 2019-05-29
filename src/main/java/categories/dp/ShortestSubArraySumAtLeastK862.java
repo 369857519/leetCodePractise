7f0e470bffa51aa15825221a12fde20f1cee4cb9
qilianshan <qilianshan@xiaomi.com>
2019-05-29 10:25:00 +0800

    not finish
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/dp/ShortestSubArraySumAtLeastK862.java
+++ src/main/java/categories/dp/ShortestSubArraySumAtLeastK862.java
@@ -9,26 +9,32 @@ import java.util.PriorityQueue;
  * Created by 祁连山 on 2019/5/29.
  */
 public class ShortestSubArraySumAtLeastK862 {
-    public int shortestSubarray(int[] A, int K) {
-        int[] sum = new int[A.length + 1];
-        sum[0] = 0;
-        for (int i = 0; i < A.length; i++) {
-            sum[i + 1] = A[i] + sum[i];
-        }
-        int min = -1;
-        //缓存一下和为sum[i]-K的所有sum，并进行排序，然后二分查找
-        Map<Integer,List<Integer>> map= new HashMap<>();
 
-        for (int i = 0; i < sum.length; i++) {
-            int j=0;
-                if (sum[i] - sum[j] >= K) {
-                    if (min == -1) {
-                        min = i - j;
-                    } else {
-                        min = Math.min(min, i - j);
-                    }
-                }
-        }
-        return min;
-    }
+	public int shortestSubarray(int[] A, int K) {
+		int[] sum = new int[A.length + 1];
+		sum[0] = 0;
+		for (int i = 0; i < A.length; i++) {
+			sum[i + 1] = A[i] + sum[i];
+		}
+		int[] sumMinusK = new int[A.length + 1];
+		for(int i=0;i<A.length;i++){
+			sumMinusK[i]=sumMinusK[i]-K;
+		}
+		int min = -1;
+		//缓存一下和小于X的最大长度
+		Map<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < sum.length; i++) {
+			for (int j = 0; j < sum.length; j++) {
+				if (sum[i] - sum[j] >= K) {
+					if (min == -1) {
+						min = i - j;
+					} else {
+						min = Math.min(min, i - j);
+					}
+				}
+			}
+		}
+		return min;
+	}
 }
