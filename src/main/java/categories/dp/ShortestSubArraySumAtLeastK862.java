8a88c175091a2ac4a7319b770db41ccc8d19b751
qilianshan <qilianshan@xiaomi.com>
2019-06-03 14:57:12 +0800

    monotonic queue
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/dp/ShortestSubArraySumAtLeastK862.java
+++ src/main/java/categories/dp/ShortestSubArraySumAtLeastK862.java
@@ -1,6 +1,8 @@
 package categories.dp;
 
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -10,31 +12,32 @@ import java.util.PriorityQueue;
  */
 public class ShortestSubArraySumAtLeastK862 {
 
-	public int shortestSubarray(int[] A, int K) {
-		int[] sum = new int[A.length + 1];
-		sum[0] = 0;
-		for (int i = 0; i < A.length; i++) {
-			sum[i + 1] = A[i] + sum[i];
-		}
-		int[] sumMinusK = new int[A.length + 1];
-		for(int i=0;i<A.length;i++){
-			sumMinusK[i]=sumMinusK[i]-K;
-		}
-		int min = -1;
-		//缓存一下和小于X的最大长度
-		Map<Integer, Integer> map = new HashMap<>();
+	public static void main(String[] args) {
+		ShortestSubArraySumAtLeastK862 shortestSubArraySumAtLeastK862 = new ShortestSubArraySumAtLeastK862();
+		int res=shortestSubArraySumAtLeastK862.shortestSubarray(new int[]{2, -1, 2}, 3);
+	}
 
-		for (int i = 0; i < sum.length; i++) {
-			for (int j = 0; j < sum.length; j++) {
-				if (sum[i] - sum[j] >= K) {
-					if (min == -1) {
-						min = i - j;
-					} else {
-						min = Math.min(min, i - j);
-					}
-				}
-			}
+	public int shortestSubarray(int[] A, int K) {
+		int N = A.length;
+		long[] P = new long[N + 1];
+		for (int i = 0; i < N; ++i) {
+			P[i + 1] = P[i] + (long) A[i];
 		}
-		return min;
+
+		int ans = N + 1;
+		Deque<Integer> monoq = new LinkedList<>();
+		for (int y = 0; y < P.length; ++y) {
+			//如果P[y]<=P[monoq.getLast()]，则说明中间有负数，加过来肯定小于k，更新monoq为y
+			//随着y的循环，检查上一项与本项的关系，能最小代价保持单调递增
+			while (!monoq.isEmpty() && P[y] <= P[monoq.getLast()]) {
+				monoq.removeLast();
+			}
+			//检查是否合法，从
+			while (!monoq.isEmpty() && P[y] >= P[monoq.getFirst()] + K) {
+				ans = Math.min(ans, y - monoq.removeFirst());
+			}
+			monoq.addLast(y);
+		}
+		return ans < N + 1 ? ans : -1;
 	}
 }
