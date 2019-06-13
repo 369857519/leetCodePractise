1c0a958b256743826daaf00fe9552ea8d9d18f3a
qilianshan <qilianshan@xiaomi.com>
2019-06-13 11:46:40 +0800

    two pointers
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/BinarySearch/BinarySearch704.java
+++ src/main/java/categories/BinarySearch/BinarySearch704.java
@@ -3,25 +3,22 @@ package categories.BinarySearch;
 public class BinarySearch704 {
 
 	public static void main(String[] args) {
-		BinarySearch704 binarySearch704=new BinarySearch704();
-		int res=binarySearch704.search(new int[]{-1,0,3,5,9,12},9);
+		BinarySearch704 binarySearch704 = new BinarySearch704();
+		int res = binarySearch704.search(new int[]{-1, 0, 3, 5, 8, 12}, 13);
 	}
-	public int search(int[] nums, int target) {
-		int low=0;
-		int high=nums.length-1;
-		while(low<=high){
-			int mid=(low+high)>>>1;
-			long midVal=nums[mid];
 
-			if(midVal<target){
-				low=mid+1;
-			}else if(midVal>target){
-				high=mid-1;
-			}else{
+	public int search(int[] nums, int target) {
+		int l = 0, r = nums.length - 1;
+		while (l <= r) {
+			int mid = l + r >>> 1;
+			if (nums[mid] < target) {
+				l = mid + 1;
+			} else if (nums[mid] > target) {
+				r = mid - 1;
+			} else {
 				return mid;
 			}
 		}
-//		return -(low+1);
-		return -1;
+		return l;
 	}
 }
