95ad289d35587d73824b59880704f7b5f974c986
haoyijing <xzhyj93@gmail.com>
2019-08-15 00:18:11 +0800

    commit list
    Signed-off-by: haoyijing <xzhyj93@gmail.com>

---
+++ src/main/java/categories/linkedList/ReverseLinkedLsit.java
@@ -0,0 +1,18 @@
+package categories.linkedList;
+
+import Utils.ListNode;
+
+public class ReverseLinkedLsit {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead=null;
+        ListNode cur=head;
+        ListNode nextCur=head;
+        while(cur!=null){
+            nextCur=cur.next;
+            cur.next=newHead;
+            newHead=cur;
+            cur=nextCur;
+        }
+        return newHead;
+    }
+}
