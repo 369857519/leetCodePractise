eb3b75cb77d6a872e42b1fc50fde5fecc69609da
qilianshan <qilianshan@xiaomi.com>
2019-08-23 10:57:38 +0800

    complete pack
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/search/partition/CoinExchange322.java
+++ src/main/java/categories/search/partition/CoinExchange322.java
@@ -4,27 +4,43 @@ public class CoinExchange322 {
 
 	public static void main(String[] args) {
 		CoinExchange322 coinExchange322 = new CoinExchange322();
-		coinExchange322.coinChange(new int[]{5, 5, 1}, 10);
+		int min = coinExchange322.coinChangeTraverse(new int[]{5, 5, 1}, 11);
+		min = coinExchange322.coinChange(new int[]{3}, 2);
+		min = coinExchange322.coinChange(new int[]{2}, 3);
 	}
 
 	public int coinChange(int[] coins, int amount) {
 		int[] memoize = new int[amount + 1];
-		for (int i = 0; i < coins.length; i++) {
-			memoize[coins[i]] = 1;
+		for (int i = 1; i < memoize.length; i++) {
+			memoize[i] = Integer.MAX_VALUE;
 		}
 		for (int i = 1; i < memoize.length; i++) {
-			int min = Integer.MAX_VALUE;
 			for (int j = 0; j < coins.length; j++) {
 				int prevIndex = i - coins[j];
-				if (prevIndex >= 0 && prevIndex < memoize.length && memoize[prevIndex] > 0) {
-					memoize[i] = Math.min(memoize[prevIndex], min);
+				if (prevIndex >= 0 && prevIndex < memoize.length) {
+					memoize[i] = Math.min(
+						(memoize[prevIndex] == Integer.MAX_VALUE ? Integer.MAX_VALUE
+							: memoize[prevIndex] + 1), memoize[i]);
 				}
 			}
-			memoize[i] = min;
 		}
-		if (memoize[amount] == 0) {
+		if (memoize[amount] == Integer.MAX_VALUE) {
 			return -1;
 		}
 		return memoize[amount];
+	}
+
+	public int coinChangeTraverse(int[] coins, int amount) {
+		if (amount == 0) {
+			return 0;
+		}
+		if (amount < 0) {
+			return 1;
+		}
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < coins.length; i++) {
+			min = Math.min(min, coinChangeTraverse(coins, amount - coins[i]));
+		}
+		return min + 1;
 	}
 }
