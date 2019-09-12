5e0f188b87d6d857f14593cac9e6ea93f0045fdc
qilianshan <qilianshan@xiaomi.com>
2019-09-12 17:25:44 +0800

    add PalindromePartitioning
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

---
+++ src/main/java/categories/string/PalindromePartitioning131.java
@@ -0,0 +1,49 @@
+package categories.string;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PalindromePartitioning131 {
+
+	public static void main(String[] args) {
+		PalindromePartitioning131 palindromePartitioning131 = new PalindromePartitioning131();
+		palindromePartitioning131.partition("aabaaacc");
+	}
+
+	public List<List<String>> partition(String s) {
+		int[][] matrix = new int[s.length()][s.length()];
+		int lastCenter = 0;
+		int lastRight = 0;
+		String countingS = "@" + String.join("#", s.split(""));
+		int[] record = new int[countingS.length()];
+		for (int i = 0; i < countingS.length(); i++) {
+			if (i < lastRight) {
+				int symmetricI = 2 * lastCenter - i;
+				record[i] = record[symmetricI];
+				if (i + record[i] >= lastRight) {
+					//从symmetricI开始重新计算
+					int count = lastRight - i;
+					while ((i - count) >= 0 && (i + count) < countingS.length()
+						&& countingS.charAt(i + count) == countingS.charAt(i - count)) {
+						count++;
+					}
+					record[i] = count;
+					lastRight = i + count - 1;
+					lastCenter = i;
+				}
+			} else {
+				int count = 1;
+				while ((i - count) >= 0 && (i + count) < countingS.length()
+					&& countingS.charAt(i + count) == countingS.charAt(i - count)) {
+					count++;
+				}
+				record[i] = count;
+				lastRight = i + count - 1;
+				lastCenter = i;
+			}
+		}
+		System.out.println(Arrays.toString(record));
+
+		return null;
+	}
+}
