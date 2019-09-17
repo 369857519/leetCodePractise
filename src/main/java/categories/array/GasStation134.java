a811f26fe83a4bebbbdef925d377882fc61cd757
qilianshan <qilianshan@xiaomi.com>
2019-09-17 18:30:00 +0800

    sort list
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/array/GasStation134.java
+++ src/main/java/categories/array/GasStation134.java
@@ -6,25 +6,28 @@ import java.util.List;
 public class GasStation134 {
 
 	public static void main(String[] args) {
-		GasStation134 gasStation134 = new GasStation134();
-		gasStation134.canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2});
-		gasStation134.canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 4, 3});
-		gasStation134.canCompleteCircuit(new int[]{1}, new int[]{0});
-		gasStation134.canCompleteCircuit(new int[]{}, new int[]{});
-		gasStation134.canCompleteCircuit(new int[]{5, 1, 2, 3, 4}, new int[]{4, 4, 1, 5, 1});
-		gasStation134.canCompleteCircuit(new int[]{2}, new int[]{2});
+//		GasStation134 gasStation134 = new GasStation134();
+//		gasStation134.canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2});
+//		gasStation134.canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 4, 3});
+//		gasStation134.canCompleteCircuit(new int[]{1}, new int[]{0});
+//		gasStation134.canCompleteCircuit(new int[]{}, new int[]{});
+//		gasStation134.canCompleteCircuit(new int[]{5, 1, 2, 3, 4}, new int[]{4, 4, 1, 5, 1});
+//		gasStation134.canCompleteCircuit(new int[]{2}, new int[]{2});
+
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.stream().filter(a -> a == 1);
 	}
 
 	public int canCompleteCircuit(int[] gas, int[] cost) {
-		List<Integer> testPoints = new ArrayList<>();
 		for (int i = 0; i < gas.length; i++) {
 			gas[i] = gas[i] - cost[i];
-			if (gas[i] >= 0) {
-				testPoints.add(i);
-			}
 		}
 		int res = -1;
-		for (Integer testPoint : testPoints) {
+		int testPoint = 0;
+
+		while (testPoint < gas.length) {
 			int i = testPoint;
 			int counter = 0;
 			for (int k = 0; k < gas.length; k++, i++) {
@@ -33,15 +36,13 @@ public class GasStation134 {
 				}
 				counter += gas[i];
 				if (counter < 0) {
+					testPoint = i;
 					break;
 				}
 				if (k == gas.length - 1) {
 					res = testPoint;
 					break;
 				}
-			}
-			if (res != -1) {
-				break;
 			}
 		}
 		return res;
