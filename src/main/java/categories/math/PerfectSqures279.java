a09e6de06479d80fe3c933551b02e840c7f13820
qilianshan <qilianshan@xiaomi.com>
2019-09-29 11:28:49 +0800

    giant
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

---
+++ src/main/java/categories/math/PerfectSqures279.java
@@ -0,0 +1,18 @@
+package categories.math;
+
+public class PerfectSqures279 {
+
+	public static void main(String[] args) {
+		PerfectSqures279 perfectSqures279=new PerfectSqures279();
+		perfectSqures279.numSquares();
+	}
+
+	public int numSquares(int n) {
+
+	}
+
+	public int traverse(){
+
+	}
+
+}
