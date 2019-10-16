c92a6267c046f07170be646eb01cd24e15803949
qilianshan <qilianshan@xiaomi.com>
2019-10-16 14:58:58 +0800

    add file
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/array/TopKFrequentElement347.java
+++ src/main/java/categories/array/TopKFrequentElement347.java
@@ -1,17 +1,21 @@
 package categories.array;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 public class TopKFrequentElement347 {
 
 	public static void main(String[] args) {
 		TopKFrequentElement347 topKFrequentElement347 = new TopKFrequentElement347();
-		topKFrequentElement347.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2);
+		topKFrequentElement347.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 1);
 		topKFrequentElement347.topKFrequent(new int[]{1, 2, 3, 4, 5}, 1);
+		topKFrequentElement347.topKFrequent(new int[]{1}, 1);
+		topKFrequentElement347.topKFrequent(new int[]{1, 2, 3, 3}, 1);
 	}
 
 	public List<Integer> topKFrequent(int[] nums, int k) {
@@ -25,9 +29,28 @@ public class TopKFrequentElement347 {
 				map.put(nums[i], store + 1);
 			}
 		}
+		Map<Integer, List<Integer>> sortedMap = new TreeMap<>(Collections.reverseOrder());
 		for (Entry<Integer, Integer> entry : map.entrySet()) {
-			if (entry.getValue() >= k) {
-				list.add(entry.getKey());
+			int newKey = entry.getValue();
+			int newValue = entry.getKey();
+			if (sortedMap.get(newKey) == null) {
+				List<Integer> storeList = new ArrayList<>();
+				storeList.add(newValue);
+				sortedMap.put(newKey, storeList);
+			} else {
+				sortedMap.get(newKey).add(newValue);
+			}
+		}
+
+		for (Entry<Integer, List<Integer>> entry : sortedMap.entrySet()) {
+			if (list.size() == k) {
+				break;
+			}
+			for (Integer num : entry.getValue()) {
+				if (list.size() == k) {
+					break;
+				}
+				list.add(num);
 			}
 		}
 		return list;
