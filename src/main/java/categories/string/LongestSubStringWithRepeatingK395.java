11313ba240331d9ba8f26cd6b847be4ca3819ca5
qilianshan <qilianshan@xiaomi.com>
2019-10-18 11:03:15 +0800

    for
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/string/LongestSubStringWithRepeatingK395.java
+++ src/main/java/categories/string/LongestSubStringWithRepeatingK395.java
@@ -1,40 +1,41 @@
 package categories.string;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class LongestSubStringWithRepeatingK395 {
 
 	public static void main(String[] args) {
 		LongestSubStringWithRepeatingK395 longestSubStringWithRepeatingK395 = new LongestSubStringWithRepeatingK395();
 		longestSubStringWithRepeatingK395.longestSubstring("aaabb", 3);
 		longestSubStringWithRepeatingK395.longestSubstring("ababbc", 2);
+		longestSubStringWithRepeatingK395.longestSubstring("ababacb", 3);
+		longestSubStringWithRepeatingK395.longestSubstring("aaabbb", 3);
+		longestSubStringWithRepeatingK395.longestSubstring("bbaaacbd", 3);
+
 	}
 
 	public int longestSubstring(String s, int k) {
-		Map<Character, Integer> counter = new HashMap<>();
+		int res = 0;
 		for (int i = 0; i < s.length(); i++) {
-			Character cur = s.charAt(i);
-			if (counter.get(cur) == null) {
-				counter.put(cur, 1);
-			} else {
-				counter.put(cur, counter.get(cur) + 1);
-			}
-		}
-		int count = 0, res = 0;
-		boolean lasting = false;
-		for (int i = 0; i < s.length(); i++) {
-			if (counter.get(s.charAt(i)) >= k) {
-				if (lasting || i == 0) {
-					count++;
-					res = Math.max(count, res);
+			int[] tempRecord = new int[26];
+			for (int j = i; j < s.length(); j++) {
+				tempRecord[s.charAt(j) - 'a']++;
+				if (j - i + 1 >= k && isLegal(tempRecord, k)) {
+					res = Math.max(res, j - i + 1);
 				}
-				lasting = true;
-			} else {
-				count = 0;
-				lasting = false;
+			}
+			for (int j = 0; j < 26; j++) {
+				tempRecord[j] = 0;
 			}
 		}
 		return res;
+	}
+
+	public boolean isLegal(int[] tempRecord, int k) {
+		boolean notEnough = true;
+		for (int j = 0; j < 26; j++) {
+			if (tempRecord[j] > 0 && tempRecord[j] < k) {
+				notEnough = false;
+			}
+		}
+		return notEnough;
 	}
 }
