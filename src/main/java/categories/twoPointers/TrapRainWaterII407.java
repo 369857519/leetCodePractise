180d9e9d3717f95ff50d3f4f58f6647bfc465b94
qilianshan <qilianshan@xiaomi.com>
2019-10-28 11:33:04 +0800

    src main
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/twoPointers/TrapRainWaterII407.java
+++ src/main/java/categories/twoPointers/TrapRainWaterII407.java
@@ -2,4 +2,16 @@ package categories.twoPointers;
 
 public class TrapRainWaterII407 {
 
+	public static void main(String[] args) {
+//  [
+//  [1,4,3,1,3,2],
+//  [3,2,1,3,2,4],
+//  [2,3,3,2,3,1]
+//  ]
+	}
+
+	public int trapRainWater(int[][] heightMap) {
+
+		return 0;
+	}
 }
