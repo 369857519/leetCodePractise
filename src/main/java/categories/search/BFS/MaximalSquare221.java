7828d1880dcf964af838b6053885e5a3c2187c14
qilianshan <qilianshan@xiaomi.com>
2019-12-04 14:13:44 +0800

    221
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/search/BFS/MaximalSquare221.java
+++ src/main/java/categories/search/BFS/MaximalSquare221.java
@@ -4,19 +4,37 @@ public class MaximalSquare221 {
 
 	public static void main(String[] args) {
 		MaximalSquare221 maximalSquare221 = new MaximalSquare221();
-		maximalSquare221.maximalSquare(new char[][]{
+		int res = maximalSquare221.maximalSquare(new char[][]{
 			{'1', '0', '1', '0', '0'},
 			{'1', '0', '1', '1', '1'},
 			{'1', '1', '1', '1', '1'},
-			{'1', '0', '0', '1', '0'}
+			{'1', '0', '1', '1', '1'}
 		});
+		res = maximalSquare221.maximalSquare(new char[][]{
+			{'1', '0', '1', '0', '0'},
+			{'1', '0', '1', '1', '1'},
+			{'1', '1', '1', '1', '1'},
+			{'1', '0', '0', '1', '0'}});
+		res = maximalSquare221.maximalSquare(new char[][]{
+			{'0', '0', '0', '1', '0', '1', '1', '1'},
+			{'0', '1', '1', '0', '0', '1', '0', '1'},
+			{'1', '0', '1', '1', '1', '1', '0', '1'},
+			{'0', '0', '0', '1', '0', '0', '0', '0'},
+			{'0', '0', '1', '0', '0', '0', '1', '0'},
+			{'1', '1', '1', '0', '0', '1', '1', '1'},
+			{'1', '0', '0', '1', '1', '0', '0', '1'},
+			{'0', '1', '0', '0', '1', '1', '0', '0'},
+			{'1', '0', '0', '1', '0', '0', '0', '0'}}
+		);
+		res = maximalSquare221.maximalSquare(new char[][]{{}});
+		res = maximalSquare221.maximalSquare(new char[][]{{'1', '0'}});
 	}
 
 	public int maximalSquare(char[][] matrix) {
 		//左上角遍历
 		int res = 0;
-		for (int i = 0; i < matrix[0].length; i++) {
-			for (int j = 0; j < matrix.length; j++) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
 				if (matrix[i][j] == '1') {
 					res = Math.max(res, leftTopConnerCount(matrix, i, j));
 				}
@@ -27,12 +45,13 @@ public class MaximalSquare221 {
 
 	public int leftTopConnerCount(char[][] matrix, int i, int j) {
 		int q = 0;
-		while (q + i < matrix[0].length && q + j < matrix.length) {
+		while (true) {
 			q++;
 			boolean currentRound = true;
-			for (int count = 0; count < q; count++) {
-				if (j + count == matrix.length || i + count == matrix[0].length
-					|| matrix[i + q][j + count] == '0' || matrix[i + count][i + q] == '0') {
+			for (int count = 0; count <= q; count++) {
+				if (j + count == matrix[0].length || i + count == matrix.length
+					|| q + i == matrix.length || q + j == matrix[0].length
+					|| matrix[i + q][j + count] == '0' || matrix[i + count][j + q] == '0') {
 					q--;
 					currentRound = false;
 					break;
