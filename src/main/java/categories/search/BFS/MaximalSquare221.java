ffd534a3f71594a84b80c6e0355558b6906d072a
qilianshan <qilianshan@xiaomi.com>
2019-12-05 17:00:52 +0800

    add file
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/search/BFS/MaximalSquare221.java
+++ src/main/java/categories/search/BFS/MaximalSquare221.java
@@ -1,5 +1,7 @@
 package categories.search.BFS;
 
+import Utils.ArrayUtil;
+
 public class MaximalSquare221 {
 
 	public static void main(String[] args) {
@@ -9,6 +11,12 @@ public class MaximalSquare221 {
 			{'1', '0', '1', '1', '1'},
 			{'1', '1', '1', '1', '1'},
 			{'1', '0', '1', '1', '1'}
+		});
+		res = maximalSquare221.maximalSquare(new char[][]{
+			{'1', '0', '1', '0', '0'},
+			{'1', '0', '1', '1', '1'},
+			{'1', '1', '1', '1', '1'},
+			{'1', '0', '0', '1', '0'}
 		});
 		res = maximalSquare221.maximalSquare(new char[][]{
 			{'1', '0', '1', '0', '0'},
@@ -31,6 +39,51 @@ public class MaximalSquare221 {
 	}
 
 	public int maximalSquare(char[][] matrix) {
+		int res = 0;
+		if (matrix.length == 0) {
+			return 0;
+		}
+		int[] memoize = new int[matrix.length + 1];
+		for (int i = 0; i < matrix[0].length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (matrix[i][j] == '1') {
+					if (i - 1 < 0 || j - 1 < 0) {
+						matrix[i][j] = 1;
+					} else {
+						memoize[i] =0;
+					}
+				} else {
+					memoize[i] = 0;
+				}
+			}
+		}
+		return 0;
+	}
+
+	public int maximalSquareWithOMNSpace(char[][] matrix) {
+		int res = 0;
+		if (matrix.length == 0) {
+			return 0;
+		}
+		int[][] memoize = new int[matrix.length][matrix[0].length];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (matrix[i][j] == '1') {
+					if (i - 1 < 0 || j - 1 < 0) {
+						memoize[i][j] = 1;
+					} else {
+						memoize[i][j] = Math.min(memoize[i - 1][j - 1],
+							Math.min(memoize[i - 1][j], memoize[i][j - 1])) + 1;
+					}
+					res = Math.max(res, memoize[i][j]);
+					ArrayUtil.printArr(memoize);
+				}
+			}
+		}
+		return res * res;
+	}
+
+	public int maximalSquareBruteForce(char[][] matrix) {
 		//左上角遍历
 		int res = 0;
 		for (int i = 0; i < matrix.length; i++) {
