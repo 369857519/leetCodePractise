4ac9fbdcf6de4efdfd80d9d33295171ac6f8ee5d
qilianshan <qilianshan@xiaomi.com>
2020-03-02 11:45:09 +0800

    update
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

---
+++ src/main/java/categories/dp/DecisionMaking/BestTimeToBuyAndSellStock121.java
@@ -0,0 +1,27 @@
+package categories.dp.DecisionMaking;
+
+public class BestTimeToBuyAndSellStock121 {
+
+    // [7,1,5,3,6,4]
+
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock121 bestTimeToBuyAndSellStock121 = new BestTimeToBuyAndSellStock121();
+//        int res = bestTimeToBuyAndSellStock121.maxProfit(new int[]{7, 1, 5, 3, 6, 4});
+        int res = bestTimeToBuyAndSellStock121.maxProfit(new int[]{5, 3, 1});
+    }
+
+    public int maxProfit(int[] prices) {
+        if(prices.length==0||prices.length==1){
+            return 0;
+        }
+        int[] dp = new int[prices.length];
+        dp[0] = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i] = Math.max(dp[i - 1], prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return dp[prices.length - 1];
+    }
+
+}
---
+++ src/main/java/categories/dp/DpStrings/PalindromicSubstrings.java
@@ -0,0 +1,39 @@
+package categories.dp.DpStrings;
+
+public class PalindromicSubstrings {
+
+    public static void main(String[] args) {
+        PalindromicSubstrings palindromicSubstrings = new PalindromicSubstrings();
+//        int res = palindromicSubstrings.countSubstrings("aaa");
+        int res = palindromicSubstrings.countSubstrings("abccc");
+
+    }
+
+    public int countSubstrings(String s) {
+        int len = s.length();
+        int[][] dp = new int[len + 1][len + 1];
+        //初始化
+        int sum = 0;
+        for (int i = 0; i < s.length(); i++) {
+            dp[i][i] = 1;
+            int j = i + 1;
+            sum++;
+            if (j < s.length() && s.charAt(i) == s.charAt(j)) {
+                dp[i][j] = 1;
+                sum++;
+            }
+        }
+        //计算
+        for (int subLen = 3; subLen <= s.length(); subLen++) {
+            for (int i = 0; i <= len - subLen; i++) {
+                if (s.charAt(i) == s.charAt(i + subLen - 1)
+                    && dp[i + 1][i + subLen - 2] != 0) {
+                    dp[i][i + subLen - 1] = 1;
+                    sum++;
+                }
+            }
+        }
+        return sum;
+    }
+
+}
