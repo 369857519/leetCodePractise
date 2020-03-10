08c069c79a4d38f53d70c6b59889f31a84c54ae3
qilianshan <qilianshan@xiaomi.com>
2020-03-10 12:01:55 +0800

    update
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/binarySearch/SearchRangePosition34.java
+++ src/main/java/categories/binarySearch/SearchRangePosition34.java
@@ -2,56 +2,70 @@ package categories.binarySearch;
 
 public class SearchRangePosition34 {
 
-	public static void main(String[] args) {
-		SearchRangePosition34 searchRangePosition35=new SearchRangePosition34();
-		int[] test=searchRangePosition35.searchRange(new int[]{1,3,5,6},2);
-		test=searchRangePosition35.searchRange(new int[]{1,3,5,6},5);
-		test=searchRangePosition35.searchRange(new int[]{1,3,5,6},6);
-		test=searchRangePosition35.searchRange(new int[]{1},0);
-		test=searchRangePosition35.searchRange(new int[]{1,3},0);
-		test=searchRangePosition35.searchRange(new int[]{5,7,7,8,8,10},8);
+    public static void main(String[] args) {
+        SearchRangePosition34 searchRangePosition35 = new SearchRangePosition34();
+        int[] test = searchRangePosition35.searchRange(new int[]{1, 1, 2}, 1);
+        test = searchRangePosition35.searchRange(new int[]{1, 3, 5, 6}, 5);
+        test = searchRangePosition35.searchRange(new int[]{1, 3, 5, 6}, 6);
+        test = searchRangePosition35.searchRange(new int[]{1}, 0);
+        test = searchRangePosition35.searchRange(new int[]{1, 3}, 0);
+        test = searchRangePosition35.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8);
 
-	}
+    }
 
-	public int[] searchRange(int[] nums, int target) {
-		int res=binarySearch(nums,0,nums.length-1,target);
-		if(res==-1){
-			return new int[]{-1,-1};
-		}
-		int right=0;
-		for(int i=res+1;i<nums.length;i++){
-			if(nums[i]==target){
-				right++;
-			}else{
-				break;
-			}
-		}
-		int left=0;
-		for(int i=res-1;i>=0;i--){
-			if(nums[i]==target){
-				left--;
-			}else{
-				break;
-			}
-		}
-		return new int[]{res+left,res+right};
+    public int[] searchRange(int[] nums, int target) {
+        int firstOne = binarySearch(nums, 0, nums.length, target);
+        if (firstOne == -1) {
+            return new int[]{-1, -1};
+        }
+        int left = 0;
+        int right = firstOne;
+        while (left < right) {
+            int mid = left + right >>> 1;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] == target) {
+                if (mid - 1 < 0 || nums[mid - 1] < target) {
+                    left = mid;
+                    break;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        int resLeft = left;
+        left = firstOne;
+        right = nums.length - 1;
+        while (left < right) {
+            int mid = left + right >>> 1;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                if (mid + 1 >= nums.length || nums[mid + 1] > target) {
+                    right = mid;
+                    break;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        int resRight = right;
+        return new int[]{resLeft, resRight};
+    }
 
-	}
+    public int binarySearch(int[] nums, int start, int end, int target) {
+        end = end - 1;
+        while (start <= end) {
+            int mid = start + end >>> 1;
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 
-	public int binarySearch(int[] nums, int start, int end, int target) {
-		if(end<0 || start> nums.length-1)return -1;
-		if(start>=end){
-			if(nums[start]==target)return start;
-			if(nums[end]==target)return end;
-			return -1;
-		}
-		int middleIndex = Math.round((end - start) / 2) + start;
-		if (target > nums[middleIndex]) {
-			return binarySearch(nums, middleIndex+1, end, target);
-		}
-		if (target < nums[middleIndex]) {
-			return binarySearch(nums, start, middleIndex-1, target);
-		}
-		return middleIndex;
-	}
 }
