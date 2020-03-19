059256ee9fcf641c66e07e9653db797aa206a7bf
qilianshan <qilianshan@xiaomi.com>
2020-03-19 14:35:32 +0800

    update
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/tree/FlatternTree114.java
+++ src/main/java/categories/tree/FlatternTree114.java
@@ -1,7 +1,6 @@
 package categories.tree;
 
 import Utils.TreeNode;
-import java.util.Stack;
 
 public class FlatternTree114 {
 
@@ -9,41 +8,29 @@ public class FlatternTree114 {
         FlatternTree114 flatternTree114 = new FlatternTree114();
         flatternTree114.flatten(TreeNode.createTreeByArr(new int[]{1, 2, 5, 3, 4, -1, 6}));
         flatternTree114.flatten(TreeNode.createTreeByArr(new int[]{1, 2}));
+        flatternTree114.flatten(TreeNode.createTreeByArr(new int[]{1, -1, 2, 3}));
     }
 
     public void flatten(TreeNode root) {
-        Stack<TreeNode> stk = new Stack<>();
-        pushStack(root, stk);
-        TreeNode head = new TreeNode(0);
-        TreeNode tail = head;
-        while (!stk.isEmpty()) {
-            TreeNode curNode = stk.pop();
-            boolean needInsert = false;
-            if (curNode.right != null) {
-                //进行一次嫁接
-                pushStack(curNode.right, stk);
-
-            }
-
-            if (curNode.left != null) {
-                curNode.right = head.right;
-                head.right = curNode;
-                curNode.left = null;
-            } else {
-                tail.right = curNode;
-                tail = curNode;
-                curNode.left = null;
-                curNode.right = null;
-            }
-            System.out.println(curNode.val);
-        }
-        System.out.println("finish");
+        TreeNode res = traverse(root);
     }
 
-    public void pushStack(TreeNode root, Stack<TreeNode> stk) {
-        while (root != null) {
-            stk.push(root);
-            root = root.left;
+    public TreeNode traverse(TreeNode node) {
+        if (node == null) {
+            return null;
         }
+        TreeNode left = traverse(node.left);
+        if (left != null) {
+            TreeNode leftHead = left;
+            TreeNode leftTail = left;
+            while (leftTail.right != null) {
+                leftTail = leftTail.right;
+            }
+            leftTail.right = node.right;
+            node.right = leftHead;
+            node.left = null;
+        }
+        traverse(node.right);
+        return node;
     }
 }
