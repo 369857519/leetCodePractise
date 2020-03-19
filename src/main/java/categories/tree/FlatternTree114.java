2d9369a212c643fa8873887af319ec04e560bf7a
qilianshan <qilianshan@xiaomi.com>
2020-03-19 12:18:22 +0800

    add file
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/tree/FlatternTree114.java
+++ src/main/java/categories/tree/FlatternTree114.java
@@ -8,30 +8,39 @@ public class FlatternTree114 {
     public static void main(String[] args) {
         FlatternTree114 flatternTree114 = new FlatternTree114();
         flatternTree114.flatten(TreeNode.createTreeByArr(new int[]{1, 2, 5, 3, 4, -1, 6}));
+        flatternTree114.flatten(TreeNode.createTreeByArr(new int[]{1, 2}));
     }
 
     public void flatten(TreeNode root) {
         Stack<TreeNode> stk = new Stack<>();
         pushStack(root, stk);
+        TreeNode head = new TreeNode(0);
+        TreeNode tail = head;
         while (!stk.isEmpty()) {
             TreeNode curNode = stk.pop();
-            boolean needInsert=false;
+            boolean needInsert = false;
             if (curNode.right != null) {
                 //进行一次嫁接
                 pushStack(curNode.right, stk);
-                if (curNode.left != null) {
-                    //嫁接
-                    System.out.println("insert" + curNode.val);
-                    needInsert=true;
-                }
-            }
-            if(needInsert){
-
-            }else{
 
             }
-            System.out.println(curNode.val);
+
+            if (curNode.left != null) {
+                curNode.right = head.right;
+                head.right = curNode;
+                curNode.left = null;
+            }
+            if (curNode.left == null && curNode.right == null) {
+                tail.right = curNode;
+                tail = curNode;
+                curNode.left = null;
+                curNode.right = null;
+            }
+            if (curNode.left == null && curNode.right != null) {
+
+            }
         }
+        System.out.println("finish");
     }
 
     public void pushStack(TreeNode root, Stack<TreeNode> stk) {
