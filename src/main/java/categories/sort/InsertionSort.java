a37fe9fa158bb2b2c35b91594281e360372cff86
qilianshan <qilianshan@xiaomi.com>
2020-03-30 15:54:03 +0800

    binary sort
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/categories/sort/InsertionSort.java
+++ src/main/java/categories/sort/InsertionSort.java
@@ -1,12 +1,14 @@
 package categories.sort;
 
 import Utils.ArrayPrintUtil;
+import java.util.Arrays;
 
 public class InsertionSort {
 
     public static void main(String[] args) {
         InsertionSort insertionSort = new InsertionSort();
         insertionSort.insertionSort(new int[]{67, 3, 3, 7, 10, 2});
+        insertionSort.binarySort(new int[]{67, 3, 3, 7, 10, 2});
     }
 
     public void insertionSort(int[] arr) {
@@ -22,24 +24,32 @@ public class InsertionSort {
     }
 
     //Binary insertion
-    public void BinarySort() {
-
+    public void binarySort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            int loc = binarySearch(arr, 0, i, temp);
+            for (; j >= loc; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = temp;
+        }
+        ArrayPrintUtil.printArr(arr);
     }
 
-    public int BinarySearch(int[] arr, int left, int right, int target) {
+    public int binarySearch(int[] arr, int left, int right, int target) {
         right = right - 1;
-        int mid = 0;
         while (left < right) {
-            mid = left + right >>> 1;
-            if (target > mid) {
+            int mid = left + right >>> 1;
+            if (target > arr[mid]) {
                 left = mid + 1;
-            } else if (target < mid) {
+            } else if (target < arr[mid]) {
                 right = mid - 1;
             } else {
-                break;
+                return mid;
             }
         }
-        return mid + left;
+        return left;
     }
 
 
