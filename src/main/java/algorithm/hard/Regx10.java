92273069c5510965f6e603ca948f2ab91a1e98d7
qilianshan <qilianshan@xiaomi.com>
2020-06-15 12:10:24 +0800

    update
    Signed-off-by: qilianshan <qilianshan@xiaomi.com>

--- src/main/java/algorithm/hard/Regx10.java
+++ src/main/java/algorithm/hard/Regx10.java
@@ -8,68 +8,31 @@ public class Regx10 {
         Regx10 regx10 = new Regx10();
 //        regx10.isMatch("aa", "a*");
 //        regx10.isMatch("ab", ".*");
-        regx10.isMatch("aab","c*a*b");
+        regx10.isMatch("aab", "c*a*b");
     }
 
     public boolean isMatch(String s, String p) {
-        int[][] record = new int[s.length()][p.length()];
+        boolean[][] record = new boolean[p.length() + 1][s.length() + 1];
         char[] sc = s.toCharArray();
         char[] pc = p.toCharArray();
+        record[0][0] = true;
+        for (int i = 1; i < pc.length + 1; i++) {
+            record[0][i] = pc[i] == '*' && (i - 2 < 0 || record[0][i - 2]);
+
+        }
         for (int i = 0; i < pc.length; i++) {
             for (int j = 0; j < sc.length; j++) {
-                if (record[i][j] == 0) {
-                    //判断是否需要补位
-                    //如果p-1,s-1，两个字符串各加一位，看加的是否能匹配
-                    if ((i - 1 < 0 && j - 1 < 0) ||
-                            (j - 1 > 0 && i - 1 > 0 && (record[i - 1][j - 1] == 1))) {
-                        if (pc[i] == '*') {
-                            //需要在判断前一位
-                            while (pc[i - 1] == sc[j]) {
-                                record[i][j] = 1;
-                                j++;
-                                if (j == sc.length) {
-                                    break;
-                                }
-                            }
-                        } else if (pc[j] == '.') {
-                            record[i][j] = 1;
-                        } else if (pc[i] == sc[j]) {
-                            record[i][j] = 1;
-                        }
-                    }
-
-                    //p-1长度的可以被匹配，目前要加一位p，判断是否能匹配
-                    if (i - 1 < 0 || (i - 1 > 0 && record[i - 1][j] == 1)) {
-                        if (pc[i] == '*') {
-                            record[i][j] = 1;
-                            //需要在判断前一位
-                            while (pc[i - 1] == sc[j] || pc[i - 1] == '.') {
-                                record[i][j] = 1;
-                                j++;
-                                if (j == sc.length) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
-
-                    //s-1长度可以被匹配，目前要加一位s，判断是否能匹配
-                    if (j - 1 < 0 || (j - 1 > 0 && record[i][j - 1] == 1)) {
-                        if (pc[i] == '*') {
-                            while (sc[j] == pc[i - 1] || pc[i - 1] == '.') {
-                                record[i][j] = 1;
-                                j++;
-                                if (j == sc.length) {
-                                    break;
-                                }
-                            }
-                        }
-                    }
+                if (pc[i] == '*') {
+                    //两种情况，一种是要前一位，一种是不要
+                }else{
+                    //如果是.的话true
+                    //如果是字母的话比较一下
                 }
-                ArrayPrintUtil.printArr(record);
+
             }
+            ArrayPrintUtil.printArr(record);
         }
-        return record[sc.length - 1][pc.length - 1] == 1;
+        return record[pc.length][sc.length];
     }
 
 }
